eb99e1ff06f685da2b8edf983a48d1ea8ba735a5
Shamiru Eshan <eshanshamiru@gmail.com>
2025-05-29 20:54:46 +0530

    Edit and fix some css

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -7,7 +7,7 @@ export const Offers = () => {
     <div className='offers'>
         <div className="offers-left">
           <h1>Exclusive</h1>
-          <h1>offers For You</h1>
+          <h1>Offers For You</h1>
           <p>ONLY ON BEST SELLERS PRODUCTS</p>
           <button>Check Now</button>
         </div>
